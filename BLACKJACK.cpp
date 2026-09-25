27e541f49f0d7272b878e7b3947bdebb03a1f588
Lasegx <lasseblucher95@gmail.com>
2026-09-25 12:38:38 +0200

    Blackjack spil test

--- BLACKJACK.cpp
+++ BLACKJACK.cpp
@@ -1,12 +1,38 @@
 ﻿// BLACKJACK.cpp : Defines the entry point for the application.
 //
-
+#include "Game.h"
+#include <iostream>
 #include "BLACKJACK.h"
 
-using namespace std;
+int main() {
+	Game game(100);
+	game.startRound(10);
 
-int main()
-{
-	cout << "Hello CMake." << endl;
+	std::cout << "== Ny runde ==\n";
+	std::cout << "Spillerens hånd: " << game.getPlayerValue() << "\n";
+	std::cout << "Dealerens hånd: " << game.getDealerValue() << "\n";
+	std::cout << "Saldo: " << game.getBalance() << "\n\n";
+
+	std::cout << "Spilleren tager et kort (hit)...\n";
+	game.playerHit();
+	std::cout << "Spillerens hånd nu: " << game.getPlayerValue() << "\n\n";
+
+	std::cout << "Spilleren stander...\n";
+	GameResult result = game.playerStand();
+
+	std::cout << "Dealerens endelige hånd: " << game.getDealerValue() << "\n";
+	std::cout << "Saldo efter runden: " << game.getBalance() << "\n";
+
+	switch (result)
+	{
+	case GameResult::PlayerWins:		std::cout << "Resultat: Spilleren vinder!\n"; break;
+	case GameResult::DealerWins:		std::cout << "Resultat: Dealeren vinder.\n"; break;
+	case GameResult::Push:				std::cout << "Resultat: Uafgjort (push).\n"; break;
+	case GameResult::PlayerBust:		std::cout << "Resultat: Spilleren buster.\n"; break;
+	case GameResult::PlayerBlackjack:	std::cout << "Resultat: Blackjack!\n"; break;
+	default:
+		break;
+	}
+
 	return 0;
 }
